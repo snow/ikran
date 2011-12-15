ea51f39e53502bbc35d54b16f26e2b1d0a9a8a0f
Snow.Hellsing <snow.hellsing@gmail.com>
2011-12-15 14:12:19 +0800

    mf

--- urls.py
+++ urls.py
@@ -4,15 +4,15 @@ from django_openid.registration import RegistrationConsumer
 #from django_openid.consumer import SessionConsumer
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('api.urls')),
     
     url(r'^', include('webapp.urls')),
